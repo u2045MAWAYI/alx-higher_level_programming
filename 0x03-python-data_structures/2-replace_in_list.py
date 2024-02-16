93f2146041c68733e2e015d9d302f5f75aa2a17d
u2045MAWAYI <u20451769@tuks.co.za>
2024-02-16 02:30:41 -0800

    data structure

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    count = 0
-    for element in my_list:
-        if count == idx:
-            return element
-        count += 1
-    return None
+def replace_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return(my_list)
