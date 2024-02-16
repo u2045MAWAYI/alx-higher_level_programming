aae27449d14d7cf189fa037e30ae3c2f9f56616e
u2045MAWAYI <u20451769@tuks.co.za>
2024-02-16 03:21:28 -0800

    data structure

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
+    for num in range(len(matrix)):
+        for j in range(len(matrix[num])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[num][j]), end='')
         print()
