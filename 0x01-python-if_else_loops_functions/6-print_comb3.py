6884a9b4555920f02d66bfdd891ceaa8abded6bc
u2045MAWAYI <u20451769@tuks.co.za>
2024-02-16 00:34:21 -0800

    loops and functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j))
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print('89')
         else:
-            print("{:d}{:d}".format(i, j), end=", ")
+            print('{}{}, '.format(x, y), end='')
