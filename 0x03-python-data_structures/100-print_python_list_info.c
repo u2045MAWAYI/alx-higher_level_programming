7b4fc94b833f9e746340950044c1867faaecdaf6
u2045MAWAYI <u20451769@tuks.co.za>
2024-02-16 05:44:02 -0800

    data structure

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints python list info
+ *
+ * @p: PyObject
+ * Return: no return
+ */
+void print_python_list_info(PyObject *p)
+{
+	long int size, num;
+	PyListObject *list;
+	PyObject *item;
+
+	size = Py_SIZE(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+
+	list = (PyListObject *)p;
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; num < size; num++)
+	{
+		item = PyList_GetItem(p, num);
+		printf("Element %ld: %s\n", num, Py_TYPE(item)->tp_name);
+	}
+}
