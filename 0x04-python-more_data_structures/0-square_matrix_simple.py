1a4d750cb60232ccee65ad07a12d7d8a3dd0c891
u2045MAWAYI <u20451769@tuks.co.za>
2024-02-18 02:01:39 -0800

    more data today

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix):
+    result_matrix = []
+    
+    for row in matrix
+        squared_row =[]
+        for element in row:
+            squared_row.append(element **2)
+        result_matrix.append(squared_row)
+    
+    return result_matrix
