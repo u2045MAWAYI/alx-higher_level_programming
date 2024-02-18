09f934b9c1a4c19cdfeb0eccd18cae95339f2e32
u2045MAWAYI <u20451769@tuks.co.za>
2024-02-18 03:50:18 -0800

    more data today

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,9 +1,56 @@
-#!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    keys_to_del = []
-    for key in a_dictionary:
-        if a_dictionary[key] == value:
-            keys_to_del.append(key)
-    for key in keys_to_del:
-        del a_dictionary[key]
-    return a_dictionary
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+#include <bytesobject.h>
+
+void print_python_list(PyObject *p) {
+    Py_ssize_t size, i;
+    PyObject *item;
+
+    if (!PyList_Check(p)) {
+        printf("  [ERROR] Invalid List Object\n");
+        return;
+    }
+
+    size = PyList_Size(p);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; ++i) {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
+void print_python_bytes(PyObject *p) {
+    Py_ssize_t size, i;
+    unsigned char *bytes;
+    PyObject *repr;
+
+    if (!PyBytes_Check(p)) {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    printf("[.] bytes object info\n");
+    printf("  [ERROR] Invalid Bytes Object\n");
+    printf("[*] Size: %ld\n", size);
+    printf("[*] Trying string: %s\n", PyBytes_AS_STRING(p));
+
+    if (size > 10)
+        size = 10;
+    else
+        printf("  [ERROR] Invalid Bytes Object\n");
+
+    bytes = (unsigned char *)PyBytes_AS_STRING(p);
+
+    printf("[*] first %ld bytes: ", size);
+    for (i = 0; i < size; ++i) {
+        printf("%02x", bytes[i]);
+        if (i < size - 1)
+            printf(" ");
+    }
+    printf("\n");
+}
