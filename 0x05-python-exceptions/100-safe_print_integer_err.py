31fdd73b64f9b170c7cbf37cc14b858d75aa1bef
u2045MAWAYI <u20451769@tuks.co.za>
2024-02-24 02:04:41 -0800

    python-exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
-        return True
-    except ValueError as e:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+        else:
+            raise TypeError("Value is not an integer")
+    except TypeError as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
